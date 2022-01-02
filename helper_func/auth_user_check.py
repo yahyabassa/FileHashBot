792479ccc311aa0beb65fe272d42abd24f058d2a
yahyabassa <42715138+yahyabassa@users.noreply.github.com>
2022-01-02 11:59:09 +0200

    Update auth_user_check.py

--- helper_func/auth_user_check.py
+++ helper_func/auth_user_check.py
@@ -1,12 +1,12 @@
 from config import Config
 
 async def AuthUserCheck(chat_id, user_id):
-    if 0 in Config.AUTH_IDS:
-        return True
-    elif user_id in Config.AUTH_IDS:
-        return True
-    elif chat_id in Config.AUTH_IDS:
-        return True
-    else:
-  #      return False
+ #   if 0 in Config.AUTH_IDS:
+ #      return True
+ #   elif user_id in Config.AUTH_IDS:
+ #       return True
+ #    elif chat_id in Config.AUTH_IDS:
+ #        return True
+ #    else:
+ #      return False
      return True
